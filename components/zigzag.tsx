ac9a93f6d76d7090ab0e5b7d7429417ee8583b35
qiming.zhang@mavenclinic.com <qiming.zhang@mavenclinic.com>
2024-02-16 18:59:52 -0500

    more commit

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -11,9 +11,9 @@ export default function Zigzag() {
         <div className="py-12 md:py-20 border-t border-gray-800">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+          <div className="max-w-3xl pb-12 md:pb-16">
             <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
-            <h1 className="h2 mb-4">One product, unlimited solutions</h1>
+            <h1 className="h2 mb-4">我们的产品</h1>
             <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
           </div>
 
